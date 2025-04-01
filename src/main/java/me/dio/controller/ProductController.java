9b665f967dd2ea282f85282ec25cf70945b329a9
Leonardo Felipe Ribas Ferreira <Leonardo_felipe1@outlook.com>
2025-04-01 12:10:44 -0300

    Create ProductController.java

---
+++ src/main/java/me/dio/controller/ProductController.java
@@ -0,0 +1,34 @@
+package com.santander.santanderdevweek.controller;
+
+import com.santander.santanderdevweek.dto.ProductDTO;
+import com.santander.santanderdevweek.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/products")
+@Validated
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    // Endpoint para criar um novo produto
+    @PostMapping
+    public ResponseEntity<ProductDTO> createProduct(@Valid @RequestBody ProductDTO productDTO) {
+        ProductDTO createdProduct = productService.createProduct(productDTO);
+        return new ResponseEntity<>(createdProduct, HttpStatus.CREATED);
+    }
+
+    // Endpoint para obter um produto por ID
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductDTO> getProduct(@PathVariable Long id) {
+        ProductDTO product = productService.getProductById(id);
+        return ResponseEntity.ok(product);
+    }
+}
